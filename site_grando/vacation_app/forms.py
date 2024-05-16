00c6c24afa312507a76fdf36283c4460dfdc874d
Сергей Балиоз <serejka50@gmail.com>
2024-05-16 23:04:25 +0300

    user

--- site_grando/vacation_app/forms.py
+++ site_grando/vacation_app/forms.py
@@ -5,11 +5,10 @@ from .models import VacationModel
 class VacationForm(forms.ModelForm):
     class Meta:
         model = VacationModel
-        fields = ['name', 'vacation_date_start', 'vacation_date_end', 'status_confirm']
+        fields = ['name', 'vacation_date_start', 'vacation_date_end']
 
         labels = {
             'name': 'ФИО',
             'vacation_date_start': 'Дата начала отпуска',
             'vacation_date_end': 'Дата окончания отпуска',
-            'status_confirm': 'Статус обработки'
         }
