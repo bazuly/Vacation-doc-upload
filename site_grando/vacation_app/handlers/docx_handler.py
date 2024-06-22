1001296fbc4ef330f814100265fc7ba92fd61c6b
Сергей Балиоз <serejka50@gmail.com>
2024-06-22 14:20:50 +0300

    vacation

---
+++ site_grando/vacation_app/handlers/docx_handler.py
@@ -0,0 +1,5 @@
+from docx import Document
+from vacation_app.models import VacationModel
+
+# возможно нужно будет реализовать, чтобы заявление сразу выгружалось в 
+# вордовский файл с данными сотрудника
