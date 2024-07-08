f27ca165a0fc1bc691b15a96e5d967993d82d71c
Сергей Балиоз <serejka50@gmail.com>
2024-07-08 12:52:31 +0300

    123

--- site_grando/about_app/views.py
+++ site_grando/about_app/views.py
@@ -3,7 +3,9 @@ from .models import AboutEmployeeModel, ReferenceBookModel
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+
 from .models import ReferenceBookModel
+from site_grando.handlers.pagination_handler import paginate_queryset
 
 
 def index_about(request):
@@ -17,18 +19,12 @@ def index_about(request):
 @login_required
 def reference_book_list(request):
     reference_book_data_list = ReferenceBookModel.objects.all()
-    item_per_page = 10
-    paginator = Paginator(reference_book_data_list, item_per_page)
-    page = request.GET.get('page')
-    try:
-        reference_book_data = paginator.page(page)
-    except PageNotAnInteger:
-        reference_book_data = paginator.page(1)
-    except EmptyPage:
-        reference_book_data = paginator.page(paginator.num_pages)
-
+    reference_book_data_paginate = paginate_queryset(
+        reference_book_data_list,
+        request
+    )
     context = {
-        'reference_book_data': reference_book_data
+        'reference_book_data': reference_book_data_paginate
     }
     return render(request, 'list_reference_book.html', context)
 
--- site_grando/hr_app/test.py
+++ site_grando/hr_app/test.py
@@ -3,3 +3,25 @@ from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from users.models import User
+
+class HrAppTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        
+    def test_upload_vacation_data_view(self):
+        url = reverse('hr_app:vacation_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list_vacation.html')
+    
+    def test_upload_vacation_data(self):
+        data = {
+            'name': 'test_vacation',
+            'vacation_date_start': '2024-05-05',
+            'vacation_date_end': '2024-06-06',
+            'status_confirm': 'На согласовании'
+        }
